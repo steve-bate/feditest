31df1e1a78a3281233bf530205bd360d1e778806
Steve Bate <svc-github@stevebate.net>
2024-05-16 08:34:16 +0200

    Comments on FediverseNode API

--- src/feditest/protocols/fediverse/__init__.py
+++ src/feditest/protocols/fediverse/__init__.py
@@ -13,24 +13,53 @@ class FediverseNode(WebFingerServer, ActivityPubNode):
     A Node that can participate in today's Fediverse.
     """
 
-    def make_create_note(self, actor_uri: str, content: str) -> str:
-        """"
-        Perform whatever actions are necessary to the actor with actor_uri will have created a Note
-        on this Node. Determine the various Note and Activity properties as per this Node's defaults.
+    def make_create_note(
+        self,
+        actor_uri: str,
+        content: str,
+        to_uri: str | None = None,
+    ) -> str:
+        """
+        TODO Consider renaming this to "publish_note" or just "publish" (for any AP object type).
+        A generic publish method could potentially handle "announce" and "reply" cases too.
+
+        Create and publish a `Note` attributed to an actor identified by `actor_uri`.
+
+        :param actor_uri: The URI of the Actor publishing the Note
+        :type actor_uri: str
+
+        :param content: The note content
+        :type content: str
+
+        :param to_uri: A recipient URI
+        :type to_uri: str
+
+        :return: The Create activity URI 
+        :rtype: str
         """
         return cast(str, self.prompt_user(
                 f'On FediverseNode "{ self.hostname }", make actor "{ actor_uri }" create a Note'
                 + ' and enter its URI when created.'
                 + f' Note content:"""\n{ content }\n"""' ))
 
-
     def wait_for_object_in_inbox(self, actor_uri: str, note_uri: str) -> str:
         """
+        TODO Consider renaming this to "wait_for_inbox_activity"
+
+        TODO The return value is dubious. There is no "local URI" for a Note.
+
+        Wait for an activity to arrive in an inbox. In some servers (e.g., Mastodon)
+        the "inbox" may not literally exist so this method must simulate it.
+
+        The node parameters will configure the detection behavior (polling interval, etc.).
+
+        :param note_uri: A Note URI (TODO Should be activity URI)
+        :return: ???
+        :raises AssertionError: The activity was not detected
         """
         return cast(str, self.prompt_user(
                 f'On FediverseNode "{ self.hostname }", wait until in actor "{ actor_uri }"\'s inbox,'
                 + f' Note with URI "{ note_uri }" has appeared and enter its local URI:'))
-
 
     def make_announce_object(self, actor_uri, note_uri: str) -> str:
         """
@@ -39,7 +68,6 @@ class FediverseNode(WebFingerServer, ActivityPubNode):
                 f'On FediverseNode "{ self.hostname }", make actor "{ actor_uri }" boost "{ note_uri }"'
                 + ' and enter the boost activity\' local URI:'))
 
-
     def make_reply(self, actor_uri, note_uri: str, reply_content: str) -> str:
         """
         """
@@ -47,7 +75,6 @@ class FediverseNode(WebFingerServer, ActivityPubNode):
                 f'On FediverseNode "{ self.hostname }", make actor "{ actor_uri }" reply to object with "{ note_uri }"'
                 + ' and enter its URI when created.'
                 + f' Reply content:"""\n{ reply_content }\n"""' ))
-
 
     def make_a_follow_b(self, a_uri_here: str, b_uri_there: str, node_there: 'ActivityPubNode') -> None:
         """
